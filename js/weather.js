9cd55013e8f7c4b673af76d1d55d101400e0a22f
bkw9603 <bkw9603@naver.com>
2021-12-31 02:39:32 +0900

    2021/12/31 fix weather API

--- js/weather.js
+++ js/weather.js
@@ -3,14 +3,14 @@ const API_KEY = "67dc96384e1bd6a312e2f6635e1ac6c1";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=matric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
-    .than((data) => {
+    .then((data) => {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
       city.innerText = data.name;
-      weather.innerText = data.weather[0].main;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 function onGeoError() {
